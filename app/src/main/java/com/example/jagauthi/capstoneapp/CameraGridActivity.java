2d17b9331d84563d42a8b1886d34d7fc2014a7e5
Joseph Gauthier <jagauthi@uark.edu>
2017-03-29 10:23:17 -0500

    Started the new camera grid activity

---
+++ app/src/main/java/com/example/jagauthi/capstoneapp/CameraGridActivity.java
@@ -0,0 +1,13 @@
+package com.example.jagauthi.capstoneapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_grid);
+    }
+}
